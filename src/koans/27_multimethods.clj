088656fdd5e1d17cb2cb2ccb78892a74114d6a80
Jefferson Silverio <jefferson.silverio@nubank.com.br>
2024-05-30 12:20:20 -0300

    Finishing exercises of file 27_transducers

--- src/koans/27_multimethods.clj
+++ src/koans/27_multimethods.clj
@@ -12,7 +12,7 @@
   (str "Hello there"))
 
 (defmulti multimethod-with-args
-  (fn [opt-one opt-two] opt-one))
+  (fn [opt-one _] opt-one))
 
 (defmethod multimethod-with-args :path-one [_ opts]
   (:first-opt opts))
@@ -27,19 +27,19 @@
 
 (meditations
  "A multimethod takes one or more arguments to dispatch on"
- (= __
+ (= "Hello, World!"
     (multimethod-without-args :first))
 
  "Though it can be ignored and represented by _ in defmethods"
- (= __
+ (= "Hello there"
     (multimethod-without-args :second))
 
  "Alternatively, we can use the arguments in defmethods"
- (= __
+ (= 1
     (multimethod-with-args :path-one {:first-opt 1
                                       :second-opt 2}))
 
  "This allows us to do something different in each method implementation"
- (= __
+ (= 6
     (multimethod-with-args :path-two {:first-opt 1
                                       :second-opt [0 1 2]})))
